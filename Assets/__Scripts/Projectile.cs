d519687347caf03718cc5c1d818d6e7a091a00cf
Alex London <londoale@umich.edu>
2021-11-19 14:52:39 -0500

    Projectile Collision Detection with Walls
    Projectiles will be destroyed when they collide with game objects tagged as "Wall".

--- Assets/__Scripts/Projectile.cs
+++ Assets/__Scripts/Projectile.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnCollisionEnter( Collision coll ) {
+        GameObject otherGO = coll.gameObject;
+
+        if (otherGO.tag == "Wall") {
+            Destroy(gameObject);
+        }
     }
 }
