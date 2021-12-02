cca2a440207faaf863a2dfb21368ff126e4fe591
M-Weiner <mweiner@emich.edu>
2021-12-02 18:30:51 -0500

    test commit
    please ignore

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -16,13 +16,10 @@ public class Player : MonoBehaviour
     public float enemyProjectileDamage = 5;
     public float fireRate = 2;
     public float ammoPack = 10;
-    public bool hasKey = false;
+    public bool hasKey1 = false;
+    public bool hasKey2 = false;
     public GameObject projectilePrefab;
     public GameObject gunEnd;
-    public GameObject damage;
-    public GameObject key;
-    public GameObject scrap;
-    public GameObject startScreen;
 
     //health, ammo and money display
     public Text healthText;
@@ -51,13 +48,6 @@ public class Player : MonoBehaviour
         }
     }
 
-    //this is for the start screen stuff!
-    void Start()
-    {
-        Time.timeScale = 0;
-    }
-
-
     void Update()
     {
         //Move player based on keyboard input  
@@ -83,13 +73,6 @@ public class Player : MonoBehaviour
 
         float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg * -1 + 90;
         transform.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
-        
-        // this is for the start screen
-        if (Input.GetKeyDown("enter") || Input.GetKeyDown("return"))
-        {
-            Time.timeScale = 1;
-            Destroy(startScreen);
-        }
 
         //Player can shoot with mouse button
         if (Input.GetMouseButtonDown(0) && Time.time > nextFire)
@@ -97,41 +80,6 @@ public class Player : MonoBehaviour
             FireGun(angle);
             ammoText.text = "Ammo : " + ammo;
         }
-        //damage color will fade
-        if(damage != null)
-        {
-            if(damage.GetComponent<Image>().color.a > 0)
-            {
-                var color = damage.GetComponent<Image>().color;
-                color.a -= 0.01f;
-
-                damage.GetComponent<Image>().color = color;
-            }
-        }
-    }
-    //function for when the player takes damage to show red
-    void gotHurt()
-    {
-        var color = damage.GetComponent<Image>().color;
-        color.a = 0.8f;
-
-        damage.GetComponent<Image>().color = color;
-
-    }
-    void Scrap()
-    {
-        var color = scrap.GetComponent<Image>().color;
-        color.a = 1f;
-        scrap.GetComponent<Image>().color = color;
-
-    }
-
-    void Key()
-    {
-        var color = key.GetComponent<Image>().color;
-        color.a = 1f;
-        key.GetComponent<Image>().color = color;
-
     }
 
     void FireGun(float angle)
@@ -169,13 +117,11 @@ public class Player : MonoBehaviour
             Destroy(otherGO);
             health -= enemyProjectileDamage;
         }
-		
-		else if(otherGO.tag == "Enemy")
-		{
-            gotHurt();
-			health -= 2;
-            healthText.text = "Health : " + health;
 
+        else if (otherGO.tag == "Enemy")
+        {
+            health -= 2;
+            healthText.text = "Health : " + health;
 
         }
         else if (otherGO.tag == "CommandTerminal") {
@@ -183,6 +129,15 @@ public class Player : MonoBehaviour
         }
         else if (otherGO.tag == "Scrap") {
             controlPopupMessage = "Press Space to Pick Up Scrap";
+        }
+        else if (otherGO.tag == "Ammo") {
+            controlPopupMessage = "Press Space to Pick Up Ammo";
+        }
+        else if (otherGO.tag == "Key") {
+            controlPopupMessage = "Press Space to Pick Up Yellow Key Pad";
+        }
+        else if (otherGO.tag == "Key2") {
+            controlPopupMessage = "Press Space to Pick Up Orange Key Pad";
         }
 
         if (health <= 0)
@@ -196,7 +151,7 @@ public class Player : MonoBehaviour
     void OnCollisionExit(Collision coll) {
         GameObject otherGO = coll.gameObject;
 
-        if (otherGO.tag == "CommandTerminal" || otherGO.tag == "Scrap") {
+        if (otherGO.tag == "CommandTerminal" || otherGO.tag == "Scrap" || otherGO.tag == "Ammo" || otherGO.tag == "Key" || otherGO.tag == "Key2") {
             controlPopupMessage = "";
         }
     }
@@ -208,18 +163,24 @@ public class Player : MonoBehaviour
 
         if (otherGO.tag == "Ammo" && Input.GetKeyDown("space")) {
             Destroy(otherGO);
+            controlPopupMessage = "";
             ammo += ammoPack;
         }
-        else if (otherGO.tag == "Key") {
+        else if (otherGO.tag == "Key" && Input.GetKeyDown("space")) {
             Destroy(otherGO);
-            hasKey = true;
-            Key();
+            controlPopupMessage = "";
+            hasKey1 = true;
+        }
+        else if (otherGO.tag == "Key2" && Input.GetKeyDown("space"))
+        {
+            Destroy(otherGO);
+            controlPopupMessage = "";
+            hasKey2 = true;
         }
         else if (otherGO.tag == "Scrap" && Input.GetKeyDown("space")) {
             Destroy(otherGO);
             controlPopupMessage = "";
             money += 100;
-            Scrap();
         }
         else if (otherGO.tag == "CommandTerminal" && Input.GetKeyDown("space")) {
             SceneManager.LoadScene("_Game_Win");
@@ -228,7 +189,6 @@ public class Player : MonoBehaviour
     }
 
 
-    // Method to open sliding doors when the player approaches them
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Door")
@@ -238,11 +198,18 @@ public class Player : MonoBehaviour
                 other.GetComponent<Door>().Moving = true;
             }
         }
-        else if (other.tag == "LockedDoor" && hasKey == true)
+        else if (other.tag == "LockedDoor" && hasKey1 == true)
         {
-            if (other.GetComponent<LockedDoor>().Moving == false)
+            if (other.GetComponent<Door>().Moving == false)
             {
-                other.GetComponent<LockedDoor>().Moving = true;
+                other.GetComponent<Door>().Moving = true;
+            }
+        }
+        else if (other.tag == "FinalDoor" && hasKey2 == true)
+        {
+            if (other.GetComponent<Door>().Moving == false)
+            {
+                other.GetComponent<Door>().Moving = true;
             }
         }
     }
